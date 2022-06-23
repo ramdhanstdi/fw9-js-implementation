afbe9b289a24f65846459d6ef058f627e5c64c77
ramdhanstdi <ramdhansetiadhi@gmail.com>
2022-06-23 08:03:51 +0700

    add Manual still bug

--- Task3.js
+++ Task3.js
@@ -18,4 +18,53 @@ const devideAndShort = (number) => {
     console.log(numEnd2);
 }
 
+const devideAndShortManual = (number) => {
+    const num = BigInt(number)
+    const numAdd = num + 1n
+    const string = numAdd+''
+    const len = string.length-1
+    let arr = []
+    let zero = []
+
+    //make string to array
+    for(let i=0; i<=len; i++){
+        zero[i] = [string[i]]
+        arr[i+1]=arr[i];
+        arr[i]= zero[i];
+        zero = []
+    }
+    //make split array
+    let newArr = []
+    let palet,palet3 = []
+    let temp,swapped
+    let a = 0
+    for(let i=0; i<=len; i++, a++){
+        if(arr[i]==0){
+            for(i=0;i<=a-1;i++){
+                swapped = false
+                for(let j=0;j<a-i;j++){
+                    if (newArr[j] > newArr[j+1]){
+                        temp = newArr[j];
+                        newArr[j] = newArr[j+1];
+                        newArr[j+1] =temp;
+                        swapped = true;
+                    }
+                    if (swapped == false){
+                        zero = []
+                        break;
+                    }
+                }
+            }
+        }else{
+            zero[i] = [arr[i]]
+            newArr[i+1]=newArr[i];
+            newArr[i]= zero[i];
+            palet = newArr
+            zero = []
+        }
+    }console.log(palet);
+}
+
 devideAndShort(98123037221069457)
+
+devideAndShortManual(98123037221069457)
